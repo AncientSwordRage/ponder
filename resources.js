88ac4a1ad12ee9ef2767eb8b4aebf206f3a9c9da
Craig Stone <pureferret@gmail.com>
2021-07-11 13:41:21 +0100

    add progressbar. Will stream data, and report on progress

--- resources.js
+++ resources.js
@@ -3,21 +3,50 @@
 const Fs = require('fs');
 const Path = require('path');
 const axios = require('axios');
-const { handleWriter } = require('./resource.util');
+const ProgressBar = require('progress')
 
-const url = 'https://mtgjson.com/api/v5/AllPrintings.json.zip'
+const url = 'https://mtgjson.com/api/v5/AllPrintings.json.zip';
+const metaUrl = 'https://mtgjson.com/api/v5/Meta.json';
 
+async function getMtgJsonVersion() {
+    return await axios(metaUrl).data.date;
+}
+
+//get json (wrapping below funcs)
+function getMtgJson() {
+    //check version vs current json file
+    //maybe axios fetch new json.zip
+    //unzip the new json.zip
+    //parse and return the json
+}
+
+//unzip json
+function unzipJson() { }
+
+//separate download, check version and unziping
 async function downloadMtgJsonZip() {
-    const path = Path.resolve(__dirname, 'resources', 'AllPrintings.json')
+    const path = Path.resolve(__dirname, 'resources', 'AllPrintings.json.zip')
     const writer = Fs.createWriteStream(path)
 
-    const response = await axios({
+    console.log('...connecting...')
+    const { data, headers } = await axios({
         url,
         method: 'GET',
         responseType: 'stream'
     })
-    if (response && response.data) response.data.pipe(writer);
-    return handleWriter(writer)
+    const totalLength = headers['content-length'];
+
+    console.log('...starting download...')
+    const progressBar = new ProgressBar('-> downloading [:bar] :percent :etas', {
+      width: 40,
+      complete: '=',
+      incomplete: ' ',
+      renderThrottle: 1,
+      total: parseInt(totalLength),
+      callback: () => console.log(`Completed in ${(new Date - progressBar.start)/1000} seconds`)
+    })
+    data.on('data', (chunk) => progressBar.tick(chunk.length))
+    data.pipe(writer);
 }
 
 module.exports = {
--- resources.spec.js
+++ resources.spec.js
@@ -1,22 +1,22 @@
 const axios = require('axios');
 const fs = require('fs');
-const { handleWriter } = require('./resource.util');
+const ProgressBar = require('progress')
 
 const { downloadMtgJsonZip, url } = require('./resources');
 
 jest.mock('fs');
 jest.mock('axios');
-jest.mock('./resource.util')
+jest.mock('progress')
 
-describe.only('fetchData', () => {
+describe('fetchData', () => {
     it('fetches successfully data from an URL', async () => {
-        const data = { status: 200, data: { pipe: () => 'data' } };
+        const onFn = jest.fn();
+        const data = { status: 200, data: { pipe: () => 'data', on: onFn }, headers:{'content-length': 100} };
 
         axios.mockImplementationOnce(() => data);
         fs.createWriteStream.mockImplementationOnce(() => 'fs');
-        handleWriter.mockImplementationOnce(() => 'data');
-        const expectedJson = await downloadMtgJsonZip();
-        expect(expectedJson).toEqual('data');
+        await downloadMtgJsonZip();
+        expect(onFn).toHaveBeenCalledWith('data', expect.any(Function));
         expect(axios).toHaveBeenCalledWith(
             expect.objectContaining({ url: 'https://mtgjson.com/api/v5/AllPrintings.json.zip' }),
         );
@@ -24,4 +24,22 @@ describe.only('fetchData', () => {
             expect.objectContaining({ responseType: 'stream' }),
         );
     });
+    it('ticks up the progress bar', async function() {
+        const tickFn = jest.fn();
+        const onFn = jest.fn((name, func) => func(['chunk']));
+        const data = { status: 200, data: { pipe: () => 'data', on: onFn }, headers:{'content-length': 1} };
+
+        ProgressBar.mockImplementationOnce(() => ({tick: tickFn}))
+        axios.mockImplementationOnce(() => data);
+        fs.createWriteStream.mockImplementationOnce(() => 'fs');
+        await downloadMtgJsonZip();
+
+        expect(ProgressBar).toHaveBeenCalledWith(
+            expect.stringContaining('downloading'),
+            expect.objectContaining({
+                total: 1
+            })
+        );
+        expect(tickFn).toHaveBeenCalledWith(1);
+    })
 });
